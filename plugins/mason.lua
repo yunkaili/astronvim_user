3425685d2110cd6476b70e80fc4d88ef107d0bc8
yunkai.li <yunkai.li@hotmail.com>
2023-12-05 20:13:00 +0800

    add lsp default

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,6 +8,9 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "lua_ls",
+        "jedi_language_server",
+        "clangd",
+        "cmake"
       })
     end,
   },
